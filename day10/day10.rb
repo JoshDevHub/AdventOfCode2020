1fd96ffa31d8775467c1e5ddf5e511355d19f4fc
jmsmith1018 <jmsmith1018@gmail.com>
2022-09-11 20:31:01 -0600

    add solution for day10 part2 in ruby

--- day10/day10.rb
+++ day10/day10.rb
@@ -2,12 +2,25 @@
 
 input = File.readlines(*ARGV, chomp: true)
             .map(&:to_i)
+            .push(0)
             .sort
-            .unshift(0)
+
+input.push(input[-1] + 3)
 
 adapter = input.each_cons(2).each_with_object(Hash.new(0)) do |(a, b), tally|
   diff = b - a
   tally[diff] += 1
 end
 
-p adapter[1] * (adapter[3] + 1)
+# Part 1 Solution
+p adapter[1] * (adapter[3]) # 2400
+
+combo_counter = Hash.new(0)
+combo_counter[0] = 1
+input.each_with_object(combo_counter) do |n, tally|
+  next if n.zero?
+
+  tally[n] = tally[n - 1] + tally[n - 2] + tally[n - 3]
+end
+
+p combo_counter[input[-1]]
